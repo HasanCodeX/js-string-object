707ddb33ae46079e03f804f73cb837847fdbdcc3
HasanCodeX <dev.hasan10@gmail.com>
2025-02-13 00:18:00 +0600

    case

---
+++ case.js
@@ -0,0 +1,28 @@
+const school = 'rajuk model';
+console.log(school.toLowerCase());
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+const subject = 'physics';
+const book = 'Physics';
+if (subject.toLowerCase() === book.toLowerCase()) {
+    console.log('I am a good learner');
+} else {
+    console.log('I am a bad boy');
+}
+if (subject === book) {
+    console.log('I am a good learner');
+} else {
+    console.log('I am a bad boy');
+}
+
+
+
+const drink =' water' ;
+const lequid = ' water '
+
+if ( drink.trim()  === lequid.trim()){
+    console .log (' water is another name of life')
+
+}
+else {console .log ('sea son')}
