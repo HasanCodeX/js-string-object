a32480fe84eab8cef75ba7babf6442697eb64e6a
HasanCodeX <dev.hasan10@gmail.com>
2025-02-12 21:18:18 +0600

    explore strings

---
+++ string.js
@@ -0,0 +1,14 @@
+const country = 'Bangladesh';
+const division = 'Dhaka'; // Capitalized for consistency
+const district = 'Kishoreganj'; // Fixed spelling
+
+// const thana = new String('thana');
+// console.log(typeof thana);
+
+const numbers = [54, 98, 78, 21, 61];
+// console.log(numbers.length);
+// console.log(numbers[1]); // Fixed the variable name
+
+const capital ='dhaka';
+console.log(capital.length)
+console.log (capital[3]);
