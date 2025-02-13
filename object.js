0b3e05233e1ca5b5f3d66546821f7d7a501c7f4b
HasanCodeX <dev.hasan10@gmail.com>
2025-02-13 20:16:07 +0600

    20_5 Introduction to Objects properties and values

---
+++ object.js
@@ -0,0 +1,28 @@
+const age = 21;
+const school = 'Guzirkuna School';
+const isPassed = true;
+let isDeveloper;
+
+console.log(isDeveloper);
+const subjects = ['Bangla', 'English', 'Math']
+const bottle_wrong =['red'];
+const bottlePrice = 15;
+const bottleDrinks ='alcohol';
+
+// object
+const bottle = {
+    brand:'iceVodka',
+    price: 25,
+    color: 'transparent',
+    isClean: false
+} 
+const subjectDetails = {
+    name : 'ict',
+    teacher : 'Mehdi',
+    examDate : '15feb',
+    chapter : ['3rd'],
+    exam: { name : 'final Exam' }
+}
+
+console.log(bottle);
+console.log(subjectDetails);
